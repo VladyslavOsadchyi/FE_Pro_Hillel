aa4b51fa053e6446950ee5ecdff8f0788fde644e
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-10-30 11:08:27 +0200

    hw_3_task_2

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-let age = 20;
-console.log(typeof age);
-
-let name = 'Vlad';
-console.log(typeof name);
-
-let isAdult = true;
-console.log(typeof isAdult);
-
-let emptyvalue = null;
-console.log(typeof emptyvalue);
-
-let value;
-console.log(typeof value);
-
-let bigInt = 121212121212121212121212121212121212122121n;
-console.log(typeof bigInt);
-
-let unigueID = Symbol("description");
-console.log(typeof unigueID);
+let firstname = "Vlad";
+let age = 39;
+let country = "Ukraine";
+let massege = `Привіт, мене звати ${firstname}, мені ${age} років, я з ${country}!`;
+console.log(massege);
