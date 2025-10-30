098025cee3ef4fb916bdefeb4380bf8276763110
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-10-30 10:41:45 +0200

    HW_3_task_1

---
+++ script.js
@@ -0,0 +1,20 @@
+let age = 20;
+console.log(typeof age);
+
+let name = 'Vlad';
+console.log(typeof name);
+
+let isAdult = true;
+console.log(typeof isAdult);
+
+let emptyvalue = null;
+console.log(typeof emptyvalue);
+
+let value;
+console.log(typeof value);
+
+let bigInt = 121212121212121212121212121212121212122121n;
+console.log(typeof bigInt);
+
+let unigueID = Symbol("description");
+console.log(typeof unigueID);
