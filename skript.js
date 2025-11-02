135f47797f71dafbfdb660f4daebff9af6c78280
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-11-02 12:59:55 +0200

    HW_4_task_4

--- skript.js
+++ skript.js
@@ -1,4 +1,28 @@
-let name = prompt(`Введіть ваше ім'я`);
-if (name !== null) {
-  alert(`Hello, ${name}. How are you?`);
+// let numOrStr = prompt("input number or string");
+// console.log(numOrStr);
+
+// if (numOrStr === null) {
+//   console.log("ви скасували");
+// } else if (numOrStr.trim() === "") {
+//   console.log("Empty String");
+// } else if (isNaN(+numOrStr)) {
+//   console.log(" number is Ba_NaN");
+// } else {
+//   console.log("OK!");
+// }
+
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
+switch (true) {
+  case (numOrStr = null):
+    console.log("ви скасували");
+    break;
+  case numOrStr.trim() === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log(" number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
 }
