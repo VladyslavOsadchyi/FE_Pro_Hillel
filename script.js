79d77f174a750300b7cfde1a9d92099f5076ca73
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-11-23 21:57:17 +0200

    HW_8_task_2

--- script.js
+++ script.js
@@ -4,15 +4,24 @@
 // console.log(sum(6)); // 10
 // console.log(sum(10)); // 20
 // console.log(sum(7)); // 27
-function counter() {
-  let startValue = 0;
-  return function (number) {
-    startValue += number;
-    return startValue;
+// function counter() {
+//   let startValue = 0;
+//   return function (number) {
+//     startValue += number;
+//     return startValue;
+//   };
+// }
+// const count = counter();
+// console.log(count(4));
+// console.log(count(6));
+// console.log(count(10));
+// console.log(count(7));
+
+// ДЗ № 8.2
+// Створити функцію для розрахунку добутку двох чисел, що викликається так: name(5)(2). Функція повинна повертати результат (у середині функції не має бути консоль лога!)
+function curryGetSum(a) {
+  return function (b) {
+    return a * b;
   };
 }
-const count = counter();
-console.log(count(4));
-console.log(count(6));
-console.log(count(10));
-console.log(count(7));
+console.log(curryGetSum(5)(2));
