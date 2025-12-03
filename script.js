8b6dae88409ea2e9d1692c35ca97a6e34b725072
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-12-03 20:27:29 +0200

    HW_11_task_2

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-// ДЗ № 11.1
-// Вивести таблицю Піфагора (10×10), таблиця повинна бути створена динамічно
-const find_result = document.querySelector(".find_result");
-const table = document.createElement("table");
-for (let row = 1; row <= 10; row++) {
-  const tr = document.createElement("tr");
-
-  for (let col = 1; col <= 10; col++) {
-    const td = document.createElement("td");
-    td.textContent = row * col;
-    tr.appendChild(td);
+// ДЗ № 11.2
+// Є блок із текстом на сторінці та кнопка. При натисканні на кнопку текст змінює колір. При повторному натисканні – повертається попередній колір
+const myText = document.querySelector("#text");
+const myButton = document.querySelector("#btn");
+function handleclick(event) {
+  if (myText.style.color === "red") {
+    myText.style.color = "";
+  } else {
+    myText.style.color = "red";
   }
-  table.appendChild(tr);
 }
-find_result.appendChild(table);
-table.querySelectorAll("td").forEach((td) => {
-  td.style.border = "1px solid black";
-  td.style.padding = "1px";
-});
+myButton.addEventListener("click", handleclick);
