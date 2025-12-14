479f10fdb01a783e83402b055460e76276abe889
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-12-14 22:46:34 +0200

    hw_12_task_3

--- script.js
+++ script.js
@@ -15,9 +15,25 @@
 // });
 
 // ДЗ 12.2. - Створіть HTML-сторінку з декількома кнопками. Ваше завдання - створити обробник подій для батьківського елементу, який відслідковуватиме кліки на всіх кнопках.
-const container = document.querySelector(".DZ_12_2");
-container.addEventListener("click", function (event) {
-  if (event.target.tagName === "BUTTON") {
-    alert("Натиснуто кнопку з id: " + event.target.id);
+// const container = document.querySelector(".DZ_12_2");
+// container.addEventListener("click", function (event) {
+//   if (event.target.tagName === "BUTTON") {
+//     alert("Натиснуто кнопку з id: " + event.target.id);
+//   }
+// });
+
+// ДЗ № 12.3 - Створіть HTML-сторінку, яка містить список завдань (to-do list) з можливістю додавання нових завдань. Ваше ціль - використовуючи делегування подій, створити обробник подій для списку завдань, який дозволить видаляти завдання при кліку на них.
+const input = document.querySelector("#newTaskInput");
+const btn = document.querySelector("#addTaskBtn");
+const taskList = document.querySelector("#taskList");
+btn.addEventListener("click", () => {
+  const text = input.value;
+  const li = document.createElement("li");
+  li.textContent = text;
+  taskList.append(li);
+});
+taskList.addEventListener("click", () => {
+  if (event.target.tagName === "LI") {
+    event.target.remove();
   }
 });
