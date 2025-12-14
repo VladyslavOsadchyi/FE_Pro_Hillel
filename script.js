331a7fbe71cabbf4078054b8be2296f8fd68fa5d
Vladyslav Osadchyi <v.osadchyy86@gmail.com>
2025-12-14 21:08:03 +0200

    hw_12_task_2

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // ДЗ 12.1. - На сторінці є дві кнопки. При натисканні на першу кнопку користувач повинен ввести в prompt посилання, при натисканні на другу – переадресовується на інший сайт (за раніше введеним посиланням).
-const container = document.querySelector(".DZ_12_1");
-let savedLink = "";
+// const container = document.querySelector(".DZ_12_1");
+// let savedLink = "";
+// container.addEventListener("click", function (event) {
+//   if (event.target.id === "setLink") {
+//     savedLink = prompt("Ввести посилання на сайт");
+//   }
+//   if (event.target.id === "goLink") {
+//     if (savedLink) {
+//       document.location.href = savedLink;
+//     } else {
+//       console.log("Спочатку введіть посилання на сайт");
+//     }
+//   }
+// });
+
+// ДЗ 12.2. - Створіть HTML-сторінку з декількома кнопками. Ваше завдання - створити обробник подій для батьківського елементу, який відслідковуватиме кліки на всіх кнопках.
+const container = document.querySelector(".DZ_12_2");
 container.addEventListener("click", function (event) {
-  if (event.target.id === "setLink") {
-    savedLink = prompt("Ввести посилання на сайт");
-  }
-  if (event.target.id === "goLink") {
-    if (savedLink) {
-      document.location.href = savedLink;
-    } else {
-      console.log("Спочатку введіть посилання на сайт");
-    }
+  if (event.target.tagName === "BUTTON") {
+    alert("Натиснуто кнопку з id: " + event.target.id);
   }
 });
